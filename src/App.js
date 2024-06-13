690b8060be4106ef3b7e7481c189e9a99e6d5e51
Gessica Goublet <gess_06570@hotmail.fr>
2024-06-13 10:12:28 +0200

    Creation page a propos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/home';
+// import Home from './pages/Home/home';
 import Headernav from '../src/components/Header/header';
 import Footer from '../src/components/Footer/footer';
 // import './styles/home.css';
-import Error from './pages/Error/error'
+// import Error from './pages/Error/error'
+import APropos from './pages/APropos/a_propos.jsx';
 
 const App = () => {
     return (
@@ -11,7 +12,8 @@ const App = () => {
             <Headernav/>
             <Routes>
                 {/* <Route path='/' element= {<Home/>}> </Route> */}
-                <Route path='/' element= {<Error/>}> </Route>
+                {/* <Route path='/' element= {<Error/>}> </Route> */}
+                <Route path='/' element= {<APropos/>}> </Route>
 
             </Routes>
             <Footer/>
