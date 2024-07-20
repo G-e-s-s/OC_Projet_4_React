b293a17efb713fa8cf37bbe5111b48d44d17dbad
Gessica Goublet <gess_06570@hotmail.fr>
2024-07-20 11:17:43 +0200

    Collapse refaite + changement btn nav

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -4,17 +4,22 @@ import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import '../Collapse/collapse.scss'
  
 function Collapse(props) {
+    const [firstTime, setFirstTime] = useState(true);
     const [rotate, setRotate] = useState(false);
-    const handleRotate = () => setRotate(!rotate);
+    const handleRotate = () => {
+      setRotate(!rotate);
+      setFirstTime(false);
+    }
     const classRotate = rotate ? "chevronUp" : "chevronDown";
     const classCard = rotate ? "showCard card-content" : "hideCard card-content";
+    const classFirstTime = firstTime ? "hide card" : "card";
 
     return (
       <div className="collapsible" onClick={handleRotate}>
         <div className="card-header">
           {props.title} <div className={classRotate} ><FontAwesomeIcon icon={faChevronUp}/> </div>
         </div>
-        <div className='card'>
+        <div className={classFirstTime}>
           <div className={classCard}>
             {props.description} 
           </div>
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,19 +2,16 @@ import React from 'react';
 import './header.scss';
 import logo from './../../assets/logo.png';
 import '../../pages/APropos/APropos';
-import {Link, useLocation} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
  
 function Header() {
-    const page = useLocation();
-    const match1 = ("/" === page.pathname || "/home" === page.pathname);
-    const match2 = ("/apropos" === page.pathname);
     return (
         <div className="navbar">
         <img className="navbar__logo" src={logo} alt='Logo Kasa'></img>
             <div className="btn-nav">
                 <nav><ul>
-                    <Link to='/'><li className={match1 ? "souligne":""} >Accueil</li></Link>
-                    <Link to='/apropos'><li className={match2 ? "souligne":""} >A Propos</li></Link>
+                    <li><NavLink to='/'>Accueil</NavLink></li>
+                    <li><NavLink to='/apropos'>A propos</NavLink></li>
                 </ul></nav>
             </div>
         </div>
