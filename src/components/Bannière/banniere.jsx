e2bbcd5fb837f3d4942c910c31042b6943195a82
Gessica Goublet <gess_06570@hotmail.fr>
2024-07-23 23:24:46 +0200

    Réctifications cartes

--- src/components/Bannière/banniere.jsx
+++ src/components/Bannière/banniere.jsx
@@ -2,22 +2,11 @@ import React from 'react';
 import "../Bannière/banniere.scss"
  
 function Banniere(props) {
-  var valeurRetour = null;
-  if(props.text === undefined){
-    valeurRetour = (
-      <div className="banniere">
-        <img className={props.className} src={props.img} alt='Paysage'></img>
-      </div>
-    );
-   } else {
-    valeurRetour = (
-      <div className="banniere">
-        <img className={props.className} src={props.img} alt='Paysage'></img>
-        <h1 className="textIntro">{props.text}</h1>
-      </div>
-    );
-   }
-
-  return valeurRetour;
+  return (
+    <div className="banniere">
+            <img className={props.className} src={props.img} alt='Paysage'></img>
+            {props.text !== undefined && <h1 className="textIntro">{props.text}</h1>}
+          </div>
+        );
 };
 export default Banniere;
